9289ad18b376266d2def7206961ef03fce6cf2c6
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-30 03:51:24 +0600

    interface type

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let student:string = "Jolly"
+
+let age: number=12
+
+let isSmart : boolean = true
+
+
+let students: string[] = ['Joe','Biden','Harris']
+
+let fees: number[] = [12,23,234,221]
+
+
+interface Person {
+
+  name:string,
+  age:number,
+  job?:string
+
+
+}
+
+let person: Person = {name: "afridi", age : 34}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/Vanila/Vanila.js
@@ -0,0 +1,12 @@
+let user = "Robert";
+
+user = 55;
+
+user = false;
+
+user = { id: 2, address: "Zinzira" };
+
+user = [1, 2, 3, 4, 5];
+
+user = () => 55;
+console.log(user);
